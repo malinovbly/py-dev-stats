fbb6fb7c4aa43e5cbfba689cffa2bf8842197ede
Новиков Артемий <temanov0530@gmail.com>
2024-12-27 02:12:55 +0500

    rename root for images uploaded from admin & fixed links areas

--- django_project/main_app/models.py
+++ django_project/main_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class VacancyDescription(models.Model):
     vacancy_name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='img/', null=True, blank=False, verbose_name='Изображение')
+    image = models.ImageField(upload_to='media/', null=True, blank=False, verbose_name='Изображение')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
