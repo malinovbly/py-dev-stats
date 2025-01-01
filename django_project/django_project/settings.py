b1ea9405c98cb543e94a5ecffc3d5093161fcb8b
Новиков Артемий <temanov0530@gmail.com>
2025-01-01 21:30:19 +0500

    added template loaders

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -69,6 +69,11 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
