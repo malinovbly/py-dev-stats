ab05385199fe92a2cd0e08a009be27a829110e45
Новиков Артемий <temanov0530@gmail.com>
2025-01-03 23:00:43 +0500

    latest vacancies page ready

--- django_project/main_app/views.py
+++ django_project/main_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import (VacancyDescription,
                      VacancyStatistics)
 
+import bs4
+import requests
+from datetime import datetime, timedelta
+
 
 def index(request):
     content = VacancyDescription.objects.get(vacancy_name='Python-программист')
@@ -11,7 +15,9 @@ def index(request):
 def general_statistics(request):
     # Динамика уровня зарплат по годам
     salary_by_year_data = VacancyStatistics.objects.get(name='Динамика уровня зарплат по годам (Все вакансии)')
-    salary_by_year_table = [d.split(': ') for d in salary_by_year_data.table_data.replace('\r\n', ' ').split(', ')]
+    salary_by_year_table = [
+        d.split(': ') for d in salary_by_year_data.table_data.replace('\r\n', ' ').split(', ')
+    ]
     for d in salary_by_year_table:
         d[1] = '{0:,}'.format(float(d[1])).replace(',', ' ')
         if (len(d[1]) - d[1].find('.') - 1) < 2:
@@ -19,13 +25,17 @@ def general_statistics(request):
 
     # Динамика количества вакансий по годам
     vacancies_count_by_year_data = VacancyStatistics.objects.get(name='Динамика количества вакансий по годам (Все вакансии)')
-    vacancies_count_by_year_table = [d.split(': ') for d in vacancies_count_by_year_data.table_data.replace('\r\n', ' ').split(', ')]
+    vacancies_count_by_year_table = [
+        d.split(': ') for d in vacancies_count_by_year_data.table_data.replace('\r\n', ' ').split(', ')
+    ]
     for d in vacancies_count_by_year_table:
         d[1] = '{0:,}'.format(int(d[1])).replace(',', ' ')
 
     # Уровень зарплат по городам (в порядке убывания)
     salary_by_area_data = VacancyStatistics.objects.get(name='Уровень зарплат по городам (Все вакансии)')
-    salary_by_area_table = [d.split(': ') for d in salary_by_area_data.table_data.replace('\r\n', ' ').split(', ')]
+    salary_by_area_table = [
+        d.split(': ') for d in salary_by_area_data.table_data.replace('\r\n', ' ').split(', ')
+    ]
     for d in salary_by_area_table:
         d[1] = '{0:,}'.format(float(d[1])).replace(',', ' ')
         if (len(d[1]) - d[1].find('.') - 1) < 2:
@@ -33,7 +43,9 @@ def general_statistics(request):
 
     # Доля вакансий по городам
     vacancies_fraction_by_area_data = VacancyStatistics.objects.get(name='Доля вакансий по городам (Все вакансии)')
-    vacancies_fraction_by_area_table = [d.split(': ') for d in vacancies_fraction_by_area_data.table_data.replace('\r\n', ' ').split(', ')]
+    vacancies_fraction_by_area_table = [
+        d.split(': ') for d in vacancies_fraction_by_area_data.table_data.replace('\r\n', ' ').split(', ')
+    ]
     for d in vacancies_fraction_by_area_table:
         if (len(d[1]) - d[1].find('.') - 1) < 3:
             d[1] += '0'
@@ -43,7 +55,9 @@ def general_statistics(request):
 
     # Топ-20 навыков по годам
     top_20_key_skills_by_year_data = VacancyStatistics.objects.get(name='Топ-20 навыков по годам (Все вакансии)')
-    top_20_key_skills_by_year_table = [td.split(': ') for td in top_20_key_skills_by_year_data.table_data.replace('\r\n', ' ').split(', ')]
+    top_20_key_skills_by_year_table = [
+        d.split(': ') for d in top_20_key_skills_by_year_data.table_data.replace('\r\n', ' ').split(', ')
+    ]
     skills_count = sum([int(d[1]) for d in top_20_key_skills_by_year_table])
     for d in top_20_key_skills_by_year_table:
         d[1] = str(round(int(d[1]) / skills_count, 3))
@@ -161,4 +175,83 @@ def skills(request):
 
 
 def latest_vacancies(request):
-    return render(request, 'latest-vacancies.html')
+    vacancy_name = VacancyDescription.objects.get(vacancy_name='Python-программист').vacancy_name
+
+    hh_api = 'https://api.hh.ru/vacancies'
+    params = {
+        'text': 'name:(Python-программист OR python OR питон OR пайтон)',
+    }
+
+    response = requests.get(hh_api, params=params)
+    latest_vacs = [vac for vac in response.json().get('items', [])]
+    latest_vacs = sorted(latest_vacs, key=lambda x: x['published_at'], reverse=True)[:10]
+
+    now = datetime.now().astimezone()
+    yesterday = now - timedelta(days=1)
+
+    recent_vacs = []
+    for vac in latest_vacs:
+        vac_date = datetime.strptime(vac['published_at'], '%Y-%m-%dT%H:%M:%S%z')
+        if yesterday <= vac_date <= now:
+            recent_vacs.append({
+                'name': vac['name'],
+                'description': get_description(vac, hh_api),
+                'key_skills': get_key_skills(vac, hh_api),
+                'employer': vac['employer']['name'],
+                'salary': get_salary(vac),
+                'area_name': vac['area']['name'],
+                'published_at': vac['published_at']
+            })
+
+    return render(
+        request,
+        'latest-vacancies.html',
+        {
+            'vacancy_name': vacancy_name,
+            'recent_vacs': recent_vacs
+        }
+    )
+
+
+##################
+# Util functions #
+##################
+
+def get_description(vac, hh_api):
+    return  requests.get(f"{hh_api}/{vac['id']}?host=hh.ru").json()['description']
+
+
+def get_key_skills(vac, hh_api):
+    result = []
+    for skill in requests.get(f"{hh_api}/{vac['id']}?host=hh.ru").json()['key_skills']:
+        result.append(skill['name'])
+    if len(result) == 0:
+        return '-'
+    return ', '.join(result)
+
+
+def get_salary(vac):
+    result = '-'
+    if vac['salary']:
+        cb_api = 'https://www.cbr.ru/scripts/XML_daily.asp'
+        date = f"01/{vac['published_at'][5:7]}/{vac['published_at'][:4]}"
+        data = requests.get(f"{cb_api}?date_req={date}")
+        soup = bs4.BeautifulSoup(data.content, 'xml')
+
+        value = 1
+        if vac['salary']['currency']:
+            for valute in soup.find_all('Valute'):
+                if valute.find('CharCode').text == vac['salary']['currency']:
+                    value = float(valute.find('VunitRate').text.replace(',', '.'))
+                    break
+        salary_from = round(value * vac['salary']['from'], 2) if vac['salary']['from'] else None
+        salary_to = round(value * vac['salary']['to'], 2) if vac['salary']['to'] else None
+
+        if salary_from and salary_to:
+            result = f'{salary_from} - {salary_to} ₽'
+        elif salary_from and not salary_to:
+            result = f'{salary_from} ₽'
+        elif salary_to and not salary_from:
+            result = f'{salary_to} ₽'
+
+    return result
