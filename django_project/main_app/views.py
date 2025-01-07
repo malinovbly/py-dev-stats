13d3a0fccb5a62b1a71bb0f255c709786af2fb63
Новиков Артемий <temanov0530@gmail.com>
2025-01-07 16:52:27 +0500

    views.py code refactor

--- django_project/main_app/views.py
+++ django_project/main_app/views.py
@@ -13,59 +13,20 @@ def index(request):
 
 
 def general_statistics(request):
-    # Динамика уровня зарплат по годам
-    salary_by_year_data = VacancyStatistics.objects.get(name='Динамика уровня зарплат по годам (Все вакансии)')
-    salary_by_year_table = [
-        d.split(': ') for d in salary_by_year_data.table_data.replace('\r\n', ' ').split(', ')
-    ]
-    for d in salary_by_year_table:
-        d[1] = '{0:,}'.format(float(d[1])).replace(',', ' ')
-        if (len(d[1]) - d[1].find('.') - 1) < 2:
-            d[1] += '0'
+    salary_by_year_data, salary_by_year_table = get_stats('Динамика уровня зарплат по годам')
+    salary_by_year_table = reformat_values(salary_by_year_table, float)
 
-    # Динамика количества вакансий по годам
-    vacancies_count_by_year_data = VacancyStatistics.objects.get(name='Динамика количества вакансий по годам (Все вакансии)')
-    vacancies_count_by_year_table = [
-        d.split(': ') for d in vacancies_count_by_year_data.table_data.replace('\r\n', ' ').split(', ')
-    ]
-    for d in vacancies_count_by_year_table:
-        d[1] = '{0:,}'.format(int(d[1])).replace(',', ' ')
+    vacancies_count_by_year_data, vacancies_count_by_year_table = get_stats('Динамика количества вакансий по годам')
+    vacancies_count_by_year_table = reformat_values(vacancies_count_by_year_table, int)
 
-    # Уровень зарплат по городам (в порядке убывания)
-    salary_by_area_data = VacancyStatistics.objects.get(name='Уровень зарплат по городам (Все вакансии)')
-    salary_by_area_table = [
-        d.split(': ') for d in salary_by_area_data.table_data.replace('\r\n', ' ').split(', ')
-    ]
-    for d in salary_by_area_table:
-        d[1] = '{0:,}'.format(float(d[1])).replace(',', ' ')
-        if (len(d[1]) - d[1].find('.') - 1) < 2:
-            d[1] += '0'
+    salary_by_area_data, salary_by_area_table = get_stats('Уровень зарплат по городам')
+    salary_by_area_table = reformat_values(salary_by_area_table, float)
 
-    # Доля вакансий по городам
-    vacancies_fraction_by_area_data = VacancyStatistics.objects.get(name='Доля вакансий по городам (Все вакансии)')
-    vacancies_fraction_by_area_table = [
-        d.split(': ') for d in vacancies_fraction_by_area_data.table_data.replace('\r\n', ' ').split(', ')
-    ]
-    for d in vacancies_fraction_by_area_table:
-        if (len(d[1]) - d[1].find('.') - 1) < 3:
-            d[1] += '0'
-        percentage = round(float(d[1]) * 100, 2)
-        d.append(f'{percentage}%')
+    vacancies_fraction_by_area_data, vacancies_fraction_by_area_table = get_stats('Доля вакансий по городам')
+    vacancies_fraction_by_area_table = reformat_values(vacancies_fraction_by_area_table, float, 3, True)
 
-
-    # Топ-20 навыков по годам
-    top_20_key_skills_by_year_data = VacancyStatistics.objects.get(name='Топ-20 навыков по годам (Все вакансии)')
-    top_20_key_skills_by_year_table = [
-        d.split(': ') for d in top_20_key_skills_by_year_data.table_data.replace('\r\n', ' ').split(', ')
-    ]
-    skills_count = sum([int(d[1]) for d in top_20_key_skills_by_year_table])
-    for d in top_20_key_skills_by_year_table:
-        d[1] = str(round(int(d[1]) / skills_count, 3))
-        if (len(d[1]) - d[1].find('.') - 1) < 3:
-            d[1] += '0'
-        percentage = round(float(d[1]) * 100, 2)
-        d.append(f'{percentage}%')
-    top_20_key_skills_by_year_table = sorted(top_20_key_skills_by_year_table, key=lambda x: x[1], reverse=True)
+    top_20_key_skills_by_year_data, top_20_key_skills_by_year_table = get_stats('Топ-20 навыков по годам')
+    top_20_key_skills_by_year_table = reformat_values(top_20_key_skills_by_year_table, float, 3, True)
 
     return render(
         request,
@@ -88,19 +49,11 @@ def general_statistics(request):
 def demand(request):
     vacancy_name = VacancyDescription.objects.get(vacancy_name='Python-программист').vacancy_name
 
-    # Динамика уровня зарплат по годам
-    salary_by_year_data = VacancyStatistics.objects.get(name='Динамика уровня зарплат по годам (Python-программист)')
-    salary_by_year_table = [d.split(': ') for d in salary_by_year_data.table_data.replace('\r\n', ' ').split(', ')]
-    for d in salary_by_year_table:
-        d[1] = '{0:,}'.format(float(d[1])).replace(',', ' ')
-        if (len(d[1]) - d[1].find('.') - 1) < 2:
-            d[1] += '0'
+    salary_by_year_data, salary_by_year_table = get_stats('Динамика уровня зарплат по годам', vacancy_name)
+    salary_by_year_table = reformat_values(salary_by_year_table, float)
 
-    # Динамика количества вакансий по годам
-    vacancies_count_by_year_data = VacancyStatistics.objects.get(name='Динамика количества вакансий по годам (Python-программист)')
-    vacancies_count_by_year_table = [d.split(': ') for d in vacancies_count_by_year_data.table_data.replace('\r\n', ' ').split(', ')]
-    for d in vacancies_count_by_year_table:
-        d[1] = '{0:,}'.format(int(d[1])).replace(',', ' ')
+    vacancies_count_by_year_data, vacancies_count_by_year_table = get_stats('Динамика количества вакансий по годам', vacancy_name)
+    vacancies_count_by_year_table = reformat_values(vacancies_count_by_year_table, int)
 
     return render(
         request,
@@ -118,22 +71,11 @@ def demand(request):
 def geography(request):
     vacancy_name = VacancyDescription.objects.get(vacancy_name='Python-программист').vacancy_name
 
-    # Уровень зарплат по городам (в порядке убывания)
-    salary_by_area_data = VacancyStatistics.objects.get(name='Уровень зарплат по городам (Python-программист)')
-    salary_by_area_table = [d.split(': ') for d in salary_by_area_data.table_data.replace('\r\n', ' ').split(', ')]
-    for d in salary_by_area_table:
-        d[1] = '{0:,}'.format(float(d[1])).replace(',', ' ')
-        if (len(d[1]) - d[1].find('.') - 1) < 2:
-            d[1] += '0'
+    salary_by_area_data, salary_by_area_table = get_stats('Уровень зарплат по городам', vacancy_name)
+    salary_by_area_table = reformat_values(salary_by_area_table, float)
 
-    # Доля вакансий по городам
-    vacancies_fraction_by_area_data = VacancyStatistics.objects.get(name='Доля вакансий по городам (Python-программист)')
-    vacancies_fraction_by_area_table = [d.split(': ') for d in vacancies_fraction_by_area_data.table_data.replace('\r\n', ' ').split(', ')]
-    for d in vacancies_fraction_by_area_table:
-        if (len(d[1]) - d[1].find('.') - 1) < 3:
-            d[1] += '0'
-        percentage = round(float(d[1]) * 100, 2)
-        d.append(f'{percentage}%')
+    vacancies_fraction_by_area_data, vacancies_fraction_by_area_table = get_stats('Доля вакансий по городам', vacancy_name)
+    vacancies_fraction_by_area_table = reformat_values(vacancies_fraction_by_area_table, float, 3, True)
 
     return render(
         request,
@@ -151,17 +93,8 @@ def geography(request):
 def skills(request):
     vacancy_name = VacancyDescription.objects.get(vacancy_name='Python-программист').vacancy_name
 
-    # Топ-20 навыков по годам
-    top_20_key_skills_by_year_data = VacancyStatistics.objects.get(name='Топ-20 навыков по годам (Python-программист)')
-    top_20_key_skills_by_year_table = [td.split(': ') for td in top_20_key_skills_by_year_data.table_data.replace('\r\n', ' ').split(', ')]
-    skills_count = sum([int(d[1]) for d in top_20_key_skills_by_year_table])
-    for d in top_20_key_skills_by_year_table:
-        d[1] = str(round(int(d[1]) / skills_count, 3))
-        if (len(d[1]) - d[1].find('.') - 1) < 3:
-            d[1] += '0'
-        percentage = round(float(d[1]) * 100, 2)
-        d.append(f'{percentage}%')
-    top_20_key_skills_by_year_table = sorted(top_20_key_skills_by_year_table, key=lambda x: x[1], reverse=True)
+    top_20_key_skills_by_year_data, top_20_key_skills_by_year_table = get_stats('Топ-20 навыков по годам', vacancy_name)
+    top_20_key_skills_by_year_table = reformat_values(top_20_key_skills_by_year_table, float, 3, True)
 
     return render(
         request,
@@ -216,6 +149,25 @@ def latest_vacancies(request):
 ##################
 # Util functions #
 ##################
+
+def get_stats(data_name, vacancy_name='Все вакансии'):
+    data = VacancyStatistics.objects.get(name=f'{data_name} ({vacancy_name})')
+    table = [
+        d.split(': ') for d in data.table_data.replace('\r\n', ' ').split(', ')
+    ]
+    return data, table
+
+
+def reformat_values(table, value_type, digits=2, add_percentage=False):
+    for d in table:
+        d[1] = '{0:,}'.format(value_type(d[1])).replace(',', ' ')
+        if value_type == float and (len(d[1]) - d[1].find('.') - 1) < digits:
+            d[1] += '0'
+        if add_percentage:
+            percentage = round(float(d[1]) * 100, 2)
+            d.append(f'{percentage}%')
+    return table
+
 
 def get_description(vac, hh_api):
     return  requests.get(f"{hh_api}/{vac['id']}?host=hh.ru").json()['description']
